4836af131aaa7df59e57a3a5b4f7d36dfa533362
tealikes <50782092+tealikes@users.noreply.github.com>
2020-06-21 22:42:23 +0300

    added my summary

---
+++ precache-manifest.574c2e4921f8b6e355795641232076b1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642362d67238b52d3108c4452392a874",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "a2548d1690f44453b7b0",
+    "url": "/cv/static/css/main.a35c1c60.chunk.css"
+  },
+  {
+    "revision": "bfc31adafae9767fc85b",
+    "url": "/cv/static/js/2.eaf0e224.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cv/static/js/2.eaf0e224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2548d1690f44453b7b0",
+    "url": "/cv/static/js/main.c21305e1.chunk.js"
+  },
+  {
+    "revision": "d6dcef095c971930b9da",
+    "url": "/cv/static/js/runtime-main.e4fe9350.js"
+  },
+  {
+    "revision": "c545cf0c9e144ca520d451ff3d09c808",
+    "url": "/cv/static/media/buk-min.c545cf0c.jpg"
+  },
+  {
+    "revision": "e1a87c08e920e066505f0d7146f21812",
+    "url": "/cv/static/media/city.e1a87c08.svg"
+  },
+  {
+    "revision": "fcf6568a67509e862b4b697ae329f19f",
+    "url": "/cv/static/media/education.fcf6568a.svg"
+  },
+  {
+    "revision": "282ce35730156a39a8e038b798b993b2",
+    "url": "/cv/static/media/email.282ce357.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/cv/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "8612839ab2ee28c3ad297e4816f06efa",
+    "url": "/cv/static/media/html5-min.8612839a.jpg"
+  },
+  {
+    "revision": "742676e4266271fe345069b9d3161e26",
+    "url": "/cv/static/media/i-min.742676e4.jpg"
+  },
+  {
+    "revision": "b67f8803a83c67e98da17c216260a8e8",
+    "url": "/cv/static/media/js-min.b67f8803.jpg"
+  },
+  {
+    "revision": "ae2860ecd6792173d63f5860f043bb75",
+    "url": "/cv/static/media/mongodb-min.ae2860ec.jpg"
+  },
+  {
+    "revision": "aa6141ae7adf71192499d4f4fe34fb6c",
+    "url": "/cv/static/media/nodejs-min.aa6141ae.jpg"
+  },
+  {
+    "revision": "0c4f5fb4e0f91012406fcf32593e6259",
+    "url": "/cv/static/media/phone.0c4f5fb4.svg"
+  },
+  {
+    "revision": "9263256cf7627f9da90aed596d3a337c",
+    "url": "/cv/static/media/reactjs-min.9263256c.jpg"
+  },
+  {
+    "revision": "a2874cf11f114dee290af3833a9c7656",
+    "url": "/cv/static/media/skype.a2874cf1.svg"
+  },
+  {
+    "revision": "2de580943ccd954929d747f459d6a473",
+    "url": "/cv/static/media/whatsApp.2de58094.svg"
+  }
+]);
