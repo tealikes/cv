f265b5051f2a416c12bc98ba0b100959fd41c869
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-10-26 17:14:48 +0300

    Added effect parallax

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App(){
     this.querySelectorAll('.layer').forEach(layer => {
       const speed = layer.getAttribute('data-speed')
 
-      const x = (window.innerWidth - e.clientX * speed)/220
-      const y = (window.innerHeight - e.clientY * speed)/220
+      const x = (window.innerWidth - e.clientX * speed)/100
+      const y = (window.innerHeight - e.clientY * speed)/100
       
       layer.style.transform = `translateX(${x}px) translateX(${y}px)`
     })
