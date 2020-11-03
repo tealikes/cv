0a9de52cef6f4a0b187ff80055f587b98b9f4dcb
Alexey V.Ryapisov <tealikes@yandex.ru>
2020-11-03 13:21:20 +0300

    Update

---
+++ public/test/storage.js
@@ -0,0 +1,12 @@
+
+
+
+function ready(){
+  let p = document.createElement('p');
+ // p.innerHTML = ''
+  const test = document.querySelector(".test")
+  // test.insertAdjacentElement('beforeend', p)
+
+}
+
+// document.addEventListener("DOMContentLoaded", ready)
